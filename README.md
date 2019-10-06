6ab308f86066f2c010073f89f6958f3c694314b3
Yu-Pin Weng (小小斌) <linuxice0609@gmail.com>
2019-10-06 16:31:58 +0800

    Roulette - 旋轉轉盤

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Unity_HelloWorld - 新增專案
 ![Unity_HelloWorld - 新增專案](./_Gif_/Unity_HelloWorld.gif)
+
+## Roulette - 旋轉轉盤
+![Roulette - 旋轉轉盤](./_Gif_/Roulette.gif)
---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotateSpeed = 0;  /* 旋轉的速率 */
+
+    // 這個是一開始的進入點 (只會執行一次)
+    void Start() { }
+
+    // 這個一個無限Loop
+    void Update()
+    {
+
+        /// 按下滑鼠左鍵 / 單點擊手機畫面 => 速度就會改成 10
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotateSpeed = 10;
+        }
+
+        /// 開始旋轉
+        transform.Rotate(0, 0, rotateSpeed);
+
+        /// 慢慢停下來
+        rotateSpeed *= 0.96f;
+    }
+}
